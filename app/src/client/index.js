17a9cadd25559b36b8236d695f0848e655a542a9
Jonny Pickard <jonathandpickard@gmail.com>
2018-08-08 23:28:42 +0100

    Add modern normalize

--- app/src/client/index.js
+++ app/src/client/index.js
@@ -1,5 +1,8 @@
+import 'modern-normalize/modern-normalize.css';
+
 import React from 'react';
 import { render } from 'react-dom';
+
 import App from 'App';
 
 render(<App />, document.getElementById('root'));
