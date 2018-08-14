34351255c28c2015a4a81b88614c4f3046e5f5bf
Jonny Pickard <jonathandpickard@gmail.com>
2018-08-14 19:30:31 +0100

    Finished testing profile photo - fixed some prop errors

--- component-library/src/components/Atoms/Image/__tests__/Image.test.js
+++ component-library/src/components/Atoms/Image/__tests__/Image.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Image from '../Image.js';
 
+const props = {
+  src: 'http://via.placeholder.com/200x200',
+  alt: 'Image Alt'
+};
+
 describe('<Image />', () => {
   it('renders succesfully', () => {
-    const wrapper = mount(<Image />);
+    const wrapper = mount(<Image {...props} />);
     expect(wrapper.find('Image')).toHaveLength(1);
   });
 });
