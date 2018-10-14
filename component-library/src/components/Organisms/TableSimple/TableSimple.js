cc292df5f780d0d137b93b21315486df3a3a776a
Jonny Pickard <jonathandpickard@gmail.com>
2018-10-14 17:36:06 +0100

    SimpleTable complete - needs testing

--- component-library/src/components/Organisms/TableSimple/TableSimple.js
+++ component-library/src/components/Organisms/TableSimple/TableSimple.js
@@ -2,6 +2,16 @@
 
 import React, { PureComponent } from 'react';
 import classNames from 'classnames';
+import get from 'lodash.get';
+
+import {
+  IconCaption,
+  iconCaptionThemeDefault as iconCaptionTheme
+} from '@jonnypickardjs/Molecules/IconCaption';
+import {
+  Heading,
+  headingThemeDefault as headingTheme
+} from '@jonnypickardjs/Atoms/Heading';
 
 import type { colorClassSelectors } from 'shared/flow-types/colorSelectorType.js';
 
@@ -11,10 +21,14 @@ type Props = {
     [css_modules_class: string]: string
   },
   extendStyle?: string,
-  cells: {
+  cells: Array<{
     backgroundColor: colorClassSelectors,
     text: string
-  }
+  }>,
+  tableKey: Array<{
+    color: colorClassSelectors,
+    text: string
+  }>
 };
 
 /**
@@ -23,15 +37,67 @@ type Props = {
 class TableSimple extends PureComponent<Props> {
   static displayName = 'TableSimple';
 
+  static defaultProps = {
+    cells: [],
+    tableKey: []
+  };
+
+  getClassSelectorForBGColor = (colorSelector: string): string => {
+    const { theme } = this.props;
+
+    return theme[`tableSimplebackgroundColors${colorSelector}`];
+  };
+
   /**
    * Render
    * @return {Element<*>} JSX
    */
   render() {
-    const { theme, extendStyle } = this.props;
+    const { theme, extendStyle, cells, tableKey, tableKeyTitle } = this.props;
+
+    const {
+      tableSimple,
+      tableSimpleCells,
+      tableSimpleCell,
+      tableSimpleKey
+    } = theme;
+
     return (
-      <div className={classNames(theme.tableSimple, extendStyle)}>
-        <h1>Hello from TableSimple</h1>
+      <div className={classNames(tableSimple, extendStyle)}>
+        <div className={tableSimpleCells}>
+          {cells.map((cell, index) => (
+            <div
+              className={classNames(
+                tableSimpleCell,
+                this.getClassSelectorForBGColor(get(cell, ['color'], ''))
+              )}
+              key={`simple-table-cell-${index}`}
+            >
+              <Heading
+                theme={headingTheme}
+                headingType="h3"
+                content={get(cell, ['text'], '')}
+              />
+            </div>
+          ))}
+        </div>
+        <div className={tableSimpleKey}>
+          <Heading
+            theme={headingTheme}
+            headingType="h2"
+            content={tableKeyTitle}
+          />
+          {tableKey.map((data, index) => (
+            <IconCaption
+              key={`simple-table-key-${index}`}
+              theme={iconCaptionTheme}
+              noIcon
+              iconBackgroundColor={get(data, ['color'], '')}
+              captionText={get(data, ['text'], '')}
+              captionSize="h3"
+            />
+          ))}
+        </div>
       </div>
     );
   }
--- component-library/src/components/Organisms/TableSimple/TableSimple.stories.js
+++ component-library/src/components/Organisms/TableSimple/TableSimple.stories.js
@@ -1,14 +1,53 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs/react';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
+
+import colors from 'style/colors/colorSelectors';
 
 import { TableSimple, tableSimpleThemeDefault } from '.';
 
-const props = {
-  theme: tableSimpleThemeDefault
-};
+const props = () => ({
+  theme: tableSimpleThemeDefault,
+  cells: [
+    {
+      text: 'Javascript',
+      color: select('Background Color - Cell 1', colors, 'mintHeavy')
+    },
+    {
+      text: 'React',
+      color: select('Background Color - Cell 2', colors, 'mintHeavy')
+    },
+    {
+      text: 'Flow Types',
+      color: select('Background Color - Cell 3', colors, 'mintHeavy')
+    },
+    {
+      text: 'Jest',
+      color: select('Background Color - Cell 4', colors, 'mintMedium')
+    },
+    {
+      text: 'Docker',
+      color: select('Background Color - Cell 5', colors, 'mintLight')
+    }
+  ],
+  tableKey: [
+    {
+      text: 'Light',
+      color: select('Background Color - Key 1', colors, 'mintLight')
+    },
+    {
+      text: 'Moderate',
+      color: select('Background Color - Key 2', colors, 'mintMedium')
+    },
+    {
+      text: 'Heavy',
+      color: select('Background Color - Key 3', colors, 'mintHeavy')
+    }
+  ],
+  tableKeyTitle: 'Usage'
+});
 
 storiesOf('Organisms/TableSimple', module)
   .addDecorator(withKnobs)
-  .add('default', () => <TableSimple {...props} />);
+  .add('default', () => <TableSimple {...props()} />);
