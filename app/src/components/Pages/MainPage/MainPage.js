53181a02b4a11b78882ffefef17b1b85a6c98642
Jonny Pickard <Jonathandpickard@gmail.com>
2018-11-07 17:36:34 +0000

    Removed unused code

--- app/src/components/Pages/MainPage/MainPage.js
+++ app/src/components/Pages/MainPage/MainPage.js
@@ -85,7 +85,6 @@ class MainPage extends Component<*, *> {
           />
         </div>
         <ProjectCardListSection projectCardListSectionTitle="Work" />
-        <ProjectCardListSection projectCardListSectionTitle="Work" />
       </div>
     );
   }
