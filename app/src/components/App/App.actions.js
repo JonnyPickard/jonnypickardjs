d8aa7cfe93c9198c0d765e46d8885d5961ffa3e4
Jonny Pickard <Jonathandpickard@gmail.com>
2018-11-07 14:54:21 +0000

    Add whatcar content

--- app/src/components/App/App.actions.js
+++ app/src/components/App/App.actions.js
@@ -2,7 +2,7 @@
 
 export const SET_VIEWPORT = 'SET_VIEWPORT';
 
-export function setViewport(viewportSize: string) {
+export function setViewport(viewportSize: 'mobile' | 'tablet' | 'desktop') {
   return {
     type: SET_VIEWPORT,
     payload: {
--- content/projects/whatcar.js
+++ content/projects/whatcar.js
@@ -1,12 +1,103 @@
 import whatcarCardImage from '@jonnypickardjs/assets/images/projectImages/whatcar-card-low-res.png';
 
-const whatcarCard = {
+const whatcarProject = {
   projectUrlTitle: 'whatcar',
   projectTitle: 'Whatcar ?',
   projectImage: {
     src: whatcarCardImage,
     alt: 'Whatcar Card'
-  }
+  },
+  roleTitle: 'Front-End Engineer',
+  dateFrom: {
+    month: 'October',
+    year: '2017'
+  },
+  dateTo: {
+    month: 'October',
+    year: '2018'
+  },
+  responsibilities: [
+    `Architecting and engineering an end to end component library solution using React
+Storybook, NPM, webpack and a variety of libraries`,
+    `Packaging and versioning components then deploying them to NPM`,
+    `Constructing a larger React application out of components using Redux Saga and React
+Router`,
+    `Creation of front-end DevOps solutions, front-end architecture and a CI/CD solution for the
+new site using Jenkins, Docker, AWS CloudFormation with ECR for versioning, and an
+intermediate Node data aggregation server.`,
+    `App and page load speed optimisation using code splitting, server-side rendering, thoughtful
+design patterns and strong AWS architectural design choices.`,
+    `Fully automated testing suite with 100% test coverage across the app and all components
+using Jest and Enzyme.`,
+    `High emphasis on code standards using eslint, prettier, stylelint, type safety with flow and
+githooks to catch mistakes early.`
+  ],
+  techUsed: [
+    {
+      title: 'React',
+      usage: 'heavy'
+    },
+    {
+      title: 'SCSS',
+      usage: 'heavy'
+    },
+    {
+      title: 'Storybook',
+      usage: 'heavy'
+    },
+    {
+      title: 'Redux',
+      usage: 'heavy'
+    },
+    {
+      title: 'Redux Saga',
+      usage: 'heavy'
+    },
+    {
+      title: 'Webpack',
+      usage: 'heavy'
+    },
+    {
+      title: 'AWS',
+      usage: 'heavy'
+    },
+    {
+      title: 'React Router',
+      usage: 'heavy'
+    },
+    {
+      title: 'Jenkins',
+      usage: 'heavy'
+    },
+    {
+      title: 'Node',
+      usage: 'heavy'
+    },
+    {
+      title: 'Invision',
+      usage: 'heavy'
+    },
+    {
+      title: 'Browser Stack',
+      usage: 'heavy'
+    },
+    {
+      title: 'Atomic Design',
+      usage: 'heavy'
+    },
+    {
+      title: 'Docker',
+      usage: 'medium'
+    },
+    {
+      title: 'Swagger',
+      usage: 'medium'
+    },
+    {
+      title: 'Sketch',
+      usage: 'light'
+    }
+  ]
 };
 
-export default whatcarCard;
+export default whatcarProject;
