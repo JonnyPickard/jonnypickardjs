301bc87abffce1a545b6998dafa21472e155645f
Jonny Pickard <Jonathandpickard@gmail.com>
2018-11-07 17:12:36 +0000

    Initial idea for fixedHero

--- app/src/components/Pages/MainPage/index.js
+++ app/src/components/Pages/MainPage/index.js
@@ -1,6 +1,8 @@
 // @flow
 
 import React, { Component } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
 
 import {
   HeroSection,
@@ -17,10 +19,48 @@ import styles from './MainPage.scss';
  * <MainPage /> component.
  */
 class MainPage extends Component<*, *> {
+  heroSectionRef: ?HTMLDivElement;
+  heroSectionRefHeight: number;
+
   static mapStateToProps = (state: Object) => {
     return {
       viewportSize: state.app.viewportSize
     };
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      fixedHero: false
+    };
+  }
+
+  componentDidMount() {
+    if (this.heroSectionRef) {
+      this.heroSectionRefHeight = this.heroSectionRef.clientHeight;
+      window.addEventListener('scroll', this.setFixedHero());
+    }
+  }
+
+  // Set a fixed hero on scroll past hero height
+  setFixedHero = () =>
+    debounce(() => {
+      if (
+        this.heroSectionRefHeight &&
+        window.pageYOffset >= this.heroSectionRefHeight
+      ) {
+        this.setState({ fixedHero: true });
+      } else if (
+        this.heroSectionRefHeight &&
+        window.pageYOffset <= this.heroSectionRefHeight
+      ) {
+        this.setState({ fixedHero: false });
+      }
+    }, 150);
+
+  setRef = (ref: ?HTMLDivElement) => {
+    this.heroSectionRef = ref;
   };
 
   render() {
@@ -30,11 +70,17 @@ class MainPage extends Component<*, *> {
 
     return (
       <div className={styles.mainPage}>
-        <HeroSection
-          theme={heroSectionThemeDefault}
-          {...heroSectionProps}
-          extendStyle={styles.mainPageHeroSection}
-        />
+        <div ref={this.setRef}>
+          <HeroSection
+            theme={heroSectionThemeDefault}
+            {...heroSectionProps}
+            extendStyle={classNames(
+              styles.mainPageHeroSection,
+              this.state.fixedHero && styles.mainPageHeroSectionFixed
+            )}
+          />
+        </div>
+        <ProjectCardListSection projectCardListSectionTitle="Work" />
         <ProjectCardListSection projectCardListSectionTitle="Work" />
       </div>
     );
