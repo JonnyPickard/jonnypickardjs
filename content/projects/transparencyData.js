452e410e99c6cd4fdbe3d961ed7c3bf34b804390
Jonny Pickard <Jonathandpickard@gmail.com>
2018-11-07 15:01:31 +0000

    Add tdata content

---
+++ content/projects/transparencyData.js
@@ -0,0 +1,47 @@
+// import tDataImage from '@jonnypickardjs/assets/images/projectImages/';
+
+const tDataImage = 'TODO';
+
+const ctmProject = {
+  projectUrlTitle: 'transparencyData',
+  projectTitle: 'Transparency Data Ltd.',
+  projectImage: {
+    src: tDataImage,
+    alt: 'Transparency Data Card'
+  },
+  roleTitle: 'Contract Swift Engineer',
+  dateFrom: {
+    month: 'June',
+    year: '2016'
+  },
+  dateTo: {
+    month: 'September',
+    year: '2016'
+  },
+  responsibilities: [
+    `Communication with the CTO around the best way to port the existing Android version over to IOS.`,
+    `Engineering the app using swift 2 with Xcode.`,
+    `Integration of third-party libraries such as Google Maps.`,
+    `Deployment and the apple/ iTunes ecosystem.`
+  ],
+  techUsed: [
+    {
+      title: 'Swift',
+      usage: 'heavy'
+    },
+    {
+      title: 'Xcode',
+      usage: 'heavy'
+    },
+    {
+      title: 'Cocoapods',
+      usage: 'heavy'
+    },
+    {
+      title: 'Google IOS SDK',
+      usage: 'heavy'
+    }
+  ]
+};
+
+export default ctmProject;
