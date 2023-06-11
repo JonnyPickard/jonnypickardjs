3ff2192945358ccdcd4181c511ba6a1d7af49fd3
Jonny Pickard <Jonathandpickard@gmail.com>
2023-06-11 14:47:30 +0100

    Block deploy + add note

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,19 @@
-const ghpages = require('gh-pages');
+const ghpages = require("gh-pages");
 
-ghpages.publish(
-  './app/build',
-  {
-    branch: 'master',
-    repo: 'git@github.com:JonnyPickard/JonnyPickard.github.io.git'
-  },
-  (err, res) => {
-    if (err) {
-      console.log(`err`, err);
-    }
-    if (res) {
-      console.log(`res`, res);
-    }
-  }
-);
+// WARNING: Do not run this. As it will deploy over GithubIO site.
+
+// ghpages.publish(
+//   './app/build',
+//   {
+//     branch: 'master',
+//     repo: 'git@github.com:JonnyPickard/JonnyPickard.github.io.git'
+//   },
+//   (err, res) => {
+//     if (err) {
+//       console.log(`err`, err);
+//     }
+//     if (res) {
+//       console.log(`res`, res);
+//     }
+//   }
+// );
